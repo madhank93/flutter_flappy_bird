c03d33ef977f74f1f5adf16cbfcc678335a92e64
madhank93 <maddymadhan946@gmail.com>
2021-03-08 19:34:37 +0530

    feat: added parallax background image

--- lib/screens/flappy_bird.dart
+++ lib/screens/flappy_bird.dart
@@ -1,9 +1,12 @@
 import 'package:flame/components/animation_component.dart';
+import 'package:flame/components/parallax_component.dart';
 import 'package:flame/game/base_game.dart';
 import 'package:flame/spritesheet.dart';
+import 'package:flutter/material.dart';
 
 class FlappyBird extends BaseGame {
   AnimationComponent _dash;
+  ParallaxComponent _parallax;
 
   FlappyBird() {
     _dash = AnimationComponent.empty();
@@ -20,6 +23,20 @@ class FlappyBird extends BaseGame {
 
     _dash.width = 120;
     _dash.height = 120;
+
+    _dash.x = 100;
+    _dash.y = 100;
+
+    _parallax = ParallaxComponent(
+      [
+        ParallaxImage("parallax/parallax-1.png"),
+        ParallaxImage("parallax/parallax-2.png", fill: LayerFill.none),
+      ],
+      baseSpeed: Offset(100, 0),
+      layerDelta: Offset(20, 0),
+    );
+
+    add(_parallax);
 
     add(_dash);
   }
