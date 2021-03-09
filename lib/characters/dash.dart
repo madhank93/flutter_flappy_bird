c3ca9280042ec04832d336bb0e6bb8596553f7ea
madhank93 <maddymadhan946@gmail.com>
2021-03-09 18:41:33 +0530

    feat: added hit animation to dash character

--- lib/characters/dash.dart
+++ lib/characters/dash.dart
@@ -1,19 +1,30 @@
 import 'dart:ui';
 
+import 'package:flame/animation.dart';
 import 'package:flame/components/animation_component.dart';
 import 'package:flame/spritesheet.dart';
 
 class Dash extends AnimationComponent {
+  Animation _flyAnimation;
+  Animation _hitAnimation;
+
   Dash() : super.empty() {
     final spriteSheet = SpriteSheet(
-      imageName: "dash-final.png",
+      imageName: "dash-sprite.png",
       textureWidth: 600,
       textureHeight: 600,
-      columns: 2, // refers to image
+
+      columns: 3, // refers to image
       rows: 1, // refers to image
     );
 
-    this.animation = spriteSheet.createAnimation(0, stepTime: .1);
+    _flyAnimation =
+        spriteSheet.createAnimation(0, from: 0, to: 2, stepTime: .1);
+
+    _hitAnimation =
+        spriteSheet.createAnimation(0, from: 1, to: 3, stepTime: .1);
+
+    this.animation = _flyAnimation;
   }
 
   @override
@@ -24,5 +35,13 @@ class Dash extends AnimationComponent {
 
     this.x = size.height / 5;
     this.y = size.width / 1.5;
+  }
+
+  void flyAnimation() {
+    this.animation = _flyAnimation;
+  }
+
+  void hitAnimation() {
+    this.animation = _hitAnimation;
   }
 }
