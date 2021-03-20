6cdfe7b0de0c489fe3f785bca876db4e43835d0f
madhank93 <maddymadhan946@gmail.com>
2021-03-20 22:24:47 +0530

    fix: app name and title name

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dash',
+      title: 'Dash Fly',
       theme: ThemeData(
         fontFamily: 'AudioWide',
         primarySwatch: Colors.blue,
